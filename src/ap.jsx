abfb331521b577c8654065f5e78107a9ccb30826
thakurashima <thakurashima23@gmail.com>
2025-08-19 12:51:01 +0530

    first commit

---
+++ src/ap.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ap(){
+
+
+    return(
+        <>
+        <div>
+            <script src="https://apis.google.com/js/api.js?onload=__iframefcb104363" type="text/javascript" charset="UTF-8" gapi_processed="true"></script>
+        </div>
+        
+        
+        </>
+    )
+}
