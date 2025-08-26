92388056340e653ed224bad0ca532a805b609a41
thakurashima <thakurashima23@gmail.com>
2025-08-26 21:20:54 +0530

    remove folder extension

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -60,7 +60,7 @@ function Home() {
         </div>
       </form>
       <div className="content row">
-        <div className="col-12 d-flex justify-content-between align-items-center ">
+        <div className="col-12 d-flex justify-content-between ">
           {/* Left Sidebar */}
           <div className="Content-left">
             <div className="col-3 w-full">
